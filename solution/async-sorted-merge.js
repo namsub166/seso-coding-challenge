94cc121d46f37d3e8c6c32997ce8f1161f891c1b
Nam Tran <tranna@dickinson.edu>
2022-12-16 08:24:41 -0800

    Add async and sync sorting functionality

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,56 @@
 "use strict";
 
+const Heap = require('heap');
+
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+// This function with pop the oldest log entries from all the active log sources, and put into the entryHeap.
+// Factor this function out to make the code cleaner.
+const popAsync = async (logSources, minHeap) => {
+  return Promise.all(
+    logSources.map(logSource =>
+      logSource.popAsync().then((logEntry) => {
+        if (logEntry == false) {
+          return;
+        }
+        if (logEntry.date < minHeap.peek().date) {
+          return;
+        }
+        minHeap.push(logEntry);
+      })
+    )
+  );
+};
+
+module.exports = async (logSources, printer) => {
+  // Similar to the sync version, we want to have an min heap to store the entry logs.
+  // Log will be sorted based on the date
+  const entryHeap = new Heap(function(log1, log2) {
+    return log1.date - log2.date;
   });
+  // Initialize the entry heap with oldest log entry from each log source
+  let activeLogSources = logSources.filter(logSource => !logSource.drained);
+  await Promise.all(
+    logSources.map(
+      logSource => logSource.popAsync().then((logEntry) => entryHeap.push(logEntry))
+  ));
+
+  while (entryHeap.size() != 0) {
+    // We do 2 steps here:
+    // 1. Add the current oldest log entry from each active (not drained) log source to the entryHeap
+    // 2. Process a certain amount of logs in the entryHeap. These guarantee to be in the sorted order.
+    await popAsync(activeLogSources, entryHeap);
+    activeLogSources = logSources.filter(logSource => !logSource.drained);
+    for (let i = 0; i < Math.floor(entryHeap.size() / activeLogSources.length); i++) {
+      printer.print(entryHeap.pop());
+    }
+    // If we ever encounter no active log sources, just flush out all the logs in the entryHeap.
+    if (activeLogSources.length == 0) {
+      while (entryHeap.size() != 0) {
+        printer.print(entryHeap.pop());
+      }
+    }
+  }
+  printer.done();
+  return Promise.resolve(console.log("Async sort complete."));
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,40 @@
 "use strict";
 
+const Heap = require('heap');
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  // We know that all the entries in each source are in chronological order, 
+  // so in order to sort all entries across all of the sources, we could 
+  // keep track of the oldest log of each source, say in A.
+  // 
+  // For each step, we have to find the oldest log in A (from log source B), and add element
+  // from B.pop() to A. 
+  // 
+  // Since for A, we want to sort based on the timestamp, and pop the oldest element, 
+  // we could use a heap data structure for A. 
+
+  // Initialize heap with key comparator is the log entry date
+  const entryHeap = new Heap(function(log1, log2) {
+    return log1.date - log2.date;
+  });
+  // Initialize heap element with the oldest log in each sources.
+  // In addition to the logEntry, we also want to keep track of where this log comes from.
+  logSources.map((logSource, index) => 
+    entryHeap.push({...logSource.pop(), logSourceIndex: index})
+  )
+  // Iterate through the heap, popping out the oldest element (heap pop), and add the next element
+  while (entryHeap.size() != 0) {
+    const oldestLogEntry = entryHeap.pop();
+    printer.print(oldestLogEntry);
+    // Add new log from the source we just printed. If we drained that log source, then do nothing
+    const sourceIndex = oldestLogEntry.logSourceIndex;
+    const newLogEntry = logSources[sourceIndex].pop();
+    if (newLogEntry) {
+      entryHeap.push({...newLogEntry, logSourceIndex: sourceIndex});  
+    }
+  }
+  printer.done();
   return console.log("Sync sort complete.");
 };
